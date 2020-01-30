e9b64bbe1a1cf46f69be3bf5770b57f0bb597971
Freddy Montes <freddymontes@gmail.com>
2020-01-30 15:07:30 -0600

    Add knobs to dot-multi-select

--- src/components/dot-multi-select/dot-multi-select.stories.js
+++ src/components/dot-multi-select/dot-multi-select.stories.js
@@ -1,11 +1,59 @@
 import readme from './readme.md';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 export default {
     title: "Custom Fields",
+    decorators: [withKnobs],
     parameters: {
         notes: readme
     }
 };
 
-export const MultiSelect = () =>
-    `<dot-multi-select label="Label" options="Pizza|pizza,Burguer|burguer,Sushi|sushi" />`;
+export const DateTime = () => {
+    const props = [
+        {
+            name: 'value',
+            content: text('Value', '')
+        },
+        {
+            name: 'name',
+            content: text('Name', 'field-name')
+        },
+        {
+            name: 'label',
+            content: text('Label', 'Label')
+        },
+        {
+            name: 'hint',
+            content: text('Hint', 'Hello Im a hint')
+        },
+        {
+            name: 'options',
+            content: text('Options', 'Pizza|pizza,Burguer|burguer,Sushi|sushi')
+        },
+        {
+            name: 'required',
+            content: boolean('Required', false)
+        },
+        {
+            name: 'requiredMessage',
+            content: text('Required Message', '')
+        },
+        {
+            name: 'disabled',
+            content: boolean('Disabled', false)
+        },
+        {
+            name: 'size',
+            content: text('Size', '')
+        }
+    ];
+
+    const datetime = document.createElement('dot-multi-select');
+
+    props.forEach(({ name, content }) => {
+        datetime[name] = content;
+    });
+
+    return datetime;
+};
