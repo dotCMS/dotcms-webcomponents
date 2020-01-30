13d3b1294461ff77a75f3994be90425d225ef851
Freddy Montes <freddymontes@gmail.com>
2020-01-30 14:45:53 -0600

    Add knobs to dot-error-message

--- src/components/dot-error-message/dot-error-message.stories.js
+++ src/components/dot-error-message/dot-error-message.stories.js
@@ -1,10 +1,12 @@
 import readme from './readme.md';
+import { withKnobs, text, } from '@storybook/addon-knobs';
 
 export default {
     title: 'Custom Fields',
+    decorators: [withKnobs],
     parameters: {
         notes: readme
     }
 };
 
-export const ErrorMessage = () => `<dot-error-message>Error</dot-error-message>`;
+export const ErrorMessage = () => `<dot-error-message>${text('Content', 'Hola')}</dot-error-message>`;
