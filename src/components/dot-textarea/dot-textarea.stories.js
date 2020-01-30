c5b8bb3fdbee6d90f1cafa5a9c0fe169b2820d30
Freddy Montes <freddymontes@gmail.com>
2020-01-30 15:35:57 -0600

    Add knobs to dot-textarea and dot-time

--- src/components/dot-textarea/dot-textarea.stories.js
+++ src/components/dot-textarea/dot-textarea.stories.js
@@ -1,10 +1,59 @@
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import readme from './readme.md';
 
 export default {
-    title: "Custom Fields",
+    title: 'Custom Fields',
+    decorators: [withKnobs],
     parameters: {
         notes: readme
     }
 };
 
-export const Textarea = () => `<dot-textarea label="Label" />`;
+export const TextArea = () => {
+    const props = [
+        {
+            name: 'value',
+            content: text('Value', '')
+        },
+        {
+            name: 'name',
+            content: text('Name', 'field-name')
+        },
+        {
+            name: 'label',
+            content: text('Label', 'Label')
+        },
+        {
+            name: 'hint',
+            content: text('Hint', 'Hello Im a hint')
+        },
+        {
+            name: 'required',
+            content: boolean('Required', false)
+        },
+        {
+            name: 'requiredMessage',
+            content: text('Required Message', '')
+        },
+        {
+            name: 'validationMessage',
+            content: text('Validation Message', '')
+        },
+        {
+            name: 'disabled',
+            content: boolean('Disabled', false)
+        },
+        {
+            name: 'regexCheck',
+            content: text('Regex Check', '')
+        }
+    ];
+
+    const textarea = document.createElement('dot-textarea');
+
+    props.forEach(({ name, content }) => {
+        textarea[name] = content;
+    });
+
+    return textarea;
+};
