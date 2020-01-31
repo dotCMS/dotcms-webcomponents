99ea765b830ee7731dfcc128c653bc8710dd4ba1
Freddy Montes <freddy@dotcms.com>
2020-01-31 15:11:17 -0600

    Reorganize folders and clean up (#18)
    * Move custom fields to its own folder
    * Run prettier
    * Add Elements folder
    * Organize stories

---
+++ src/elements/dot-contentlet-thumbnail/dot-contentlet-thumbnail.tsx
@@ -0,0 +1,11 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'dot-contentlet-thumbnail',
+    styleUrl: 'dot-contentlet-thumbnail.scss'
+})
+export class DotContentletThumbnail {
+    render() {
+        return <h1>Hello</h1>;
+    }
+}
