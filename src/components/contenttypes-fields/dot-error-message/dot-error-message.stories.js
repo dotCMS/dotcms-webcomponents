c7d5bb360f1df2c126b6ba649e1fc55c1452cd56
Freddy Montes <freddymontes@gmail.com>
2020-01-31 13:45:59 -0600

    Move custom fields to its own folder

---
+++ src/components/contenttypes-fields/dot-error-message/dot-error-message.stories.js
@@ -0,0 +1,12 @@
+import readme from './readme.md';
+import { withKnobs, text, } from '@storybook/addon-knobs';
+
+export default {
+    title: 'Custom Fields',
+    decorators: [withKnobs],
+    parameters: {
+        notes: readme
+    }
+};
+
+export const ErrorMessage = () => `<dot-error-message>${text('Content', 'This is an error message for fields and form')}</dot-error-message>`;
