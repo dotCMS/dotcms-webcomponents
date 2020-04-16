5c59d07a321ac7b22605c0b89a1a25b8adbff127
Freddy Montes <freddymontes@gmail.com>
2020-04-16 15:15:28 -0600

    Fix typo

--- src/components.d.ts
+++ src/components.d.ts
@@ -592,7 +592,7 @@ export namespace Components {
     /**
     * Reset properties of the field, clear value and emit events.
     */
-    'reset': () => void;
+    'reset': () => Promise<void>;
     /**
     * Value of the field
     */
@@ -726,7 +726,7 @@ export namespace Components {
     * Reset properties of the field, clear value and emit events.
     * @memberof DotSelectComponent
     */
-    'reset': () => void;
+    'reset': () => Promise<void>;
     /**
     * Value set from the dropdown option
     */
