ca20b7bccccbcdc116841ec549bc2635b15dad21
Humberto Morera <humberto.morera@dotcms.com>
2021-02-03 17:40:16 -0600

    progress

--- src/components.d.ts
+++ src/components.d.ts
@@ -629,6 +629,28 @@ export namespace Components {
          */
         "required": boolean;
     }
+    interface DotMaterialIcon {
+        /**
+          * Color value set from the input
+         */
+        "colorValue": string;
+        /**
+          * Name that will be used as ID
+         */
+        "name": string;
+        /**
+          * Value for input placeholder
+         */
+        "placeholder": string;
+        /**
+          * Values that the auto-complete textbox should search for
+         */
+        "suggestionlist": string[];
+        /**
+          * Value set from the dropdown option
+         */
+        "value": string;
+    }
     interface DotMultiSelect {
         /**
           * (optional) Disables field's interaction
@@ -1175,6 +1197,12 @@ declare global {
         prototype: HTMLDotLabelElement;
         new (): HTMLDotLabelElement;
     };
+    interface HTMLDotMaterialIconElement extends Components.DotMaterialIcon, HTMLStencilElement {
+    }
+    var HTMLDotMaterialIconElement: {
+        prototype: HTMLDotMaterialIconElement;
+        new (): HTMLDotMaterialIconElement;
+    };
     interface HTMLDotMultiSelectElement extends Components.DotMultiSelect, HTMLStencilElement {
     }
     var HTMLDotMultiSelectElement: {
@@ -1281,6 +1309,7 @@ declare global {
         "dot-input-calendar": HTMLDotInputCalendarElement;
         "dot-key-value": HTMLDotKeyValueElement;
         "dot-label": HTMLDotLabelElement;
+        "dot-material-icon": HTMLDotMaterialIconElement;
         "dot-multi-select": HTMLDotMultiSelectElement;
         "dot-progress-bar": HTMLDotProgressBarElement;
         "dot-radio": HTMLDotRadioElement;
@@ -1906,6 +1935,29 @@ declare namespace LocalJSX {
          */
         "required"?: boolean;
     }
+    interface DotMaterialIcon {
+        /**
+          * Color value set from the input
+         */
+        "colorValue"?: string;
+        /**
+          * Name that will be used as ID
+         */
+        "name"?: string;
+        "onDotValueChange"?: (event: CustomEvent<{ name: string; value: string; colorValue: string }>) => void;
+        /**
+          * Value for input placeholder
+         */
+        "placeholder"?: string;
+        /**
+          * Values that the auto-complete textbox should search for
+         */
+        "suggestionlist"?: string[];
+        /**
+          * Value set from the dropdown option
+         */
+        "value"?: string;
+    }
     interface DotMultiSelect {
         /**
           * (optional) Disables field's interaction
@@ -2312,6 +2364,7 @@ declare namespace LocalJSX {
         "dot-input-calendar": DotInputCalendar;
         "dot-key-value": DotKeyValue;
         "dot-label": DotLabel;
+        "dot-material-icon": DotMaterialIcon;
         "dot-multi-select": DotMultiSelect;
         "dot-progress-bar": DotProgressBar;
         "dot-radio": DotRadio;
@@ -2358,6 +2411,7 @@ declare module "@stencil/core" {
             "dot-input-calendar": LocalJSX.DotInputCalendar & JSXBase.HTMLAttributes<HTMLDotInputCalendarElement>;
             "dot-key-value": LocalJSX.DotKeyValue & JSXBase.HTMLAttributes<HTMLDotKeyValueElement>;
             "dot-label": LocalJSX.DotLabel & JSXBase.HTMLAttributes<HTMLDotLabelElement>;
+            "dot-material-icon": LocalJSX.DotMaterialIcon & JSXBase.HTMLAttributes<HTMLDotMaterialIconElement>;
             "dot-multi-select": LocalJSX.DotMultiSelect & JSXBase.HTMLAttributes<HTMLDotMultiSelectElement>;
             "dot-progress-bar": LocalJSX.DotProgressBar & JSXBase.HTMLAttributes<HTMLDotProgressBarElement>;
             "dot-radio": LocalJSX.DotRadio & JSXBase.HTMLAttributes<HTMLDotRadioElement>;
